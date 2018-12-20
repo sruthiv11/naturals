2be4f6c621a0bb78cd5a86645aa3a62eb1fce9fd
Madhuri <madhuri_mukkamala@softforceapps.com>
2018-12-20 15:54:41 +0000

    Initial commit

---
+++ Sm_Maximum/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+	map: {
+		'*': {
+			jquerybootstrap: "Sm_Maximum/js/bootstrap/bootstrap.min",
+			owlcarousel: "Sm_Maximum/js/owl.carousel",
+			jqueryfancyboxpack: "Sm_Maximum/js/jquery.fancybox.pack",
+			jqueryfancyboxmedia: "Sm_Maximum/js/jquery.fancybox-media",
+			jqueryunveil: "Sm_Maximum/js/jquery.unveil",
+			jquerywow: "Sm_Maximum/js/jquery.wow",
+			yttheme: "Sm_Maximum/js/yttheme"
+		}
+	}
+};
